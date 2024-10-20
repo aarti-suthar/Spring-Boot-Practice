f435cf86ed5a9b2557651346d0e5e99e04a7817a
aarti-suthar <09aartisuthar@gmail.com>
2024-10-20 16:25:32 +0530

    Pushing the Spring JPA practice

--- jpaTuts/src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/controllers/ProductController.java
+++ jpaTuts/src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/controllers/ProductController.java
@@ -2,8 +2,13 @@ package com.codingshuttle.jpaTutorial.jpaTuts.controllers;
 
 import com.codingshuttle.jpaTutorial.jpaTuts.entities.ProductEntity;
 import com.codingshuttle.jpaTutorial.jpaTuts.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,13 +18,49 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final int PAGE_SIZE = 5;
 
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    @GetMapping
-    public List<ProductEntity> getAllProducts(){
-        return productRepository.findByOrderByPrice();
-    }
+//    @GetMapping()
+//    public List<ProductEntity> getAllProducts(){
+//        return productRepository.findByTitleOrderByPrice("Mazza");
+//    }
+
+//    @GetMapping
+//    public List<ProductEntity> getAllProducts(){
+//        return productRepository.findByOrderByPrice();
+//    }
+
+    //using sort by to sort the data
+    //By default items are sort in ascending order
+//    @GetMapping
+//    public List<ProductEntity> getAllProducts(@RequestParam(defaultValue = "id") String sortBy) {
+//        return productRepository.findBy(Sort.by(sortBy));
+//    }
+
+    //Here is an example if you want to change the order
+//    @GetMapping
+//    public List<ProductEntity> getAllProducts(@RequestParam(defaultValue = "id") String sortBy){
+//        return productRepository.findAll(Sort.by(Sort.Direction.DESC,sortBy));
+//    }
+
+     //Another way to sort to the items
+//     @GetMapping
+//     public List<ProductEntity> getAllProducts(@RequestParam(defaultValue = "id") String sortBy) {
+//        return productRepository.findBy(Sort.by(
+//                Sort.Order.desc(sortBy),
+//                Sort.Order.asc("title")));
+//     }
+
+     //Pagination Example
+     @GetMapping
+     public List<ProductEntity> getAllProducts(@RequestParam(defaultValue= "id") String sortBy,
+                                               @RequestParam(defaultValue = "0") Integer pageNumber){
+        Pageable pageable = PageRequest.of(pageNumber, PAGE_SIZE, Sort.by(sortBy));
+        return productRepository.findAll(pageable).getContent();
+     }
 }
+
--- jpaTuts/src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/repositories/ProductRepository.java
+++ jpaTuts/src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/repositories/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.codingshuttle.jpaTutorial.jpaTuts.repositories;
 
 import com.codingshuttle.jpaTutorial.jpaTuts.entities.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
@@ -15,7 +20,14 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     //List<ProductEntity> findCreatedAtAfter(LocalDateTime of);
 
-    //List<ProductEntity> findByTitleOrderByPrice(String title);
+    List<ProductEntity> findByTitleOrderByPrice(String title);
 
     List<ProductEntity> findByOrderByPrice();
+
+    @Query("select e.title, e.price from ProductEntity e where e.title=:title and e.price=:price")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    List<ProductEntity> findBy(Sort sort);
+
+    Page<ProductEntity> findAll(Pageable pageable);
 }
